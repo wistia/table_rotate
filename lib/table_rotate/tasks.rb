9c2fae7d04239605e9fe5de3e6ae13b85d58c3f3
Max Schnur <mschnur@wistia.com>
2016-02-08 17:18:58 -0500

    Make description of rake task consistent.

--- lib/table_rotate/tasks.rb
+++ lib/table_rotate/tasks.rb
@@ -3,7 +3,7 @@ module TableRotate
     include Rake::DSL if defined?(Rake::DSL)
     def install_tasks
       namespace :table_rotate do
-        desc 'Calls MyModel.archive! on the specified ActiveRecord class'
+        desc 'Calls klass.archive! on the specified ActiveRecord class'
         task :archive, :klass do |t, args|
           klass = args[0].constantize
           klass.archive!
